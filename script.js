a886bafa04e9de97cc1ff5b7a537794048464ebb
Nouman Sharif <noumansharifgul27@gmail.com>
2024-06-11 22:44:54 +0500

    understanding process and process.argv

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-let n = 5;
+// let n = 5;
 
-for (let i = 0; i < n; i++) {
-  console.log(`Hello, ${i}`);
+// for (let i = 0; i < n; i++) {
+//   console.log(`Hello, ${i}`);
+// }
+let arguments = process.argv;
+
+for (let i = 2; i < arguments.length; i++) {
+  console.log(process.argv[i]);
 }
