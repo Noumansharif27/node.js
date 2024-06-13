8e8b0c07824ad50fe514a8e22b5b082fe00f7b5c
Nouman Sharif <noumansharifgul27@gmail.com>
2024-06-13 23:59:11 +0500

    learning about module (file/folder)

---
+++ node.js/math.js
@@ -0,0 +1,7 @@
+const sum = (a, b) => a + b;
+const mul = (a, b) => a * b;
+const div = (a, b) => a / b;
+const PI = 3.14;
+const g = 9.5;
+
+module.exports = sum;
---
+++ node.js/script.js
@@ -0,0 +1,16 @@
+// let n = 5;
+
+// for (let i = 0; i < n; i++) {
+//   console.log(`Hello, ${i}`);
+// }
+// let arguments = process.argv;
+
+// for (let i = 2; i < arguments.length; i++) {
+//   console.log(process.argv[i]);
+// }
+
+// let someValue = require("./math.js");
+// console.log(someValue(5, 9));
+
+let info = require("./fruits");
+console.log(info);
